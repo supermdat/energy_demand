c36a46fceadc700e806725c981a1ece111e83928
supermdat <turse.mda@gmail.com>
2021-10-06 00:58:37 -0400

    Running targets file as a test. Updating needed librarie, and subsequently, the lock file.

--- Src/TEST_running_targets.R
+++ Src/TEST_running_targets.R
@@ -3,3 +3,12 @@
 library(targets)
 
 targets::tar_make()
+
+targets::tar_visnetwork()
+targets::tar_visnetwork(label = "time")
+targets::tar_visnetwork(label = "branches")
+targets::tar_visnetwork(label = c("time", "branches"))
+
+targets::tar_read(name = "get_data")
+
+
